4bb519a1881527d516e8736fe24473c373d939c8
chabakeawChan <nattapong.sa@outlook.com>
2025-04-05 10:19:18 +0700

    Add ability to abort in-progress download

--- ui.py
+++ ui.py
@@ -15,6 +15,7 @@ class UI:
         self.queue_executioner = QueueExecutioner()
         self.image_download_manager = ImageDownloadManager()
         self.download_button_enable = True
+        self.is_manually_abort = False
 
         self.window = tkinter.Tk()
         self.window.title("PyBulk Image Downloader")
@@ -23,11 +24,13 @@ class UI:
         self.text_editor_input = tkinter.Text(width=100)
         self.urls_list_label = tkinter.Label(text="Enter urls image (one url per line)", font=LABEL_FONT)
         self.downloaded_image_label = tkinter.Label(text="Recently Downloaded", font=LABEL_FONT, anchor="w", width=30)
-        self.directory_label = tkinter.Label(text="Download to directory (If leave blank, default will be 'Downloads')", font=LABEL_FONT, anchor="w", width=90)
+        self.directory_label = tkinter.Label(text="Download to directory (If leave blank, default will be 'Downloads')", font=LABEL_FONT, anchor="w", width=70)
         self.directory_input = tkinter.Entry(width=135)
         self.directory_input.insert(0, "Downloads")
         self.canvas = tkinter.Canvas(width=320, height=480, background="white")
         self.download_btn = tkinter.Button(text="Download", width=24, command=self.start_download)
+        self.abort_btn = tkinter.Button(text="Abort", width=24, command=self.abort_download)
+        self.abort_btn.config(state="disabled")
         self.progress_bar = ttk.Progressbar(length=810)
         self.recent_downloaded_image = None
         self.messagebox = messagebox
@@ -45,8 +48,10 @@ class UI:
         self.directory_input.grid(column=0, row=3, columnspan=3, pady=(20, 0))
         self.progress_bar.grid(column=0, row=4, columnspan=3, pady=20)
         self.download_btn.grid(column=0, row=5)
+        self.abort_btn.grid(column=1, row=5)
 
     def reset_form(self):
+        self.abort_btn.config(state="disabled")
         self.text_editor_input.config(state="normal")
         self.directory_input.config(state="normal")
         self.text_editor_input.delete(index1=0.0, index2='end-1c')
@@ -54,20 +59,41 @@ class UI:
         self.directory_input.insert(0, "Downloads")
         self.download_btn.config(state="normal")
 
+    def soft_reset_form(self):
+        self.text_editor_input.config(state="normal")
+        self.directory_input.config(state="normal")
+        self.download_btn.config(state="normal")
+        self.abort_btn.config(state="disabled")
+        self.is_manually_abort = False
+        self.queue_executioner.reset_queue()
+        self.progress_bar.stop()
+
     def notify_download_complete(self):
         self.messagebox.showinfo(title="Download completed", message="All images was downloaded")
 
+    def notify_download_abort(self):
+        self.messagebox.showinfo(title="Download aborted", message="Download progress was aborted")
+
+    def abort_download(self):
+        self.is_manually_abort = True
+
     def start_download(self):
+        self.abort_btn.config(state="normal")
         self.download_btn.config(state="disabled")
         self.text_editor_input.config(state="disabled")
         self.directory_input.config(state="disabled")
-        self.queue_executioner.exec_queues(text_input=self.text_editor_input.get(index1=0.0, index2='end-1c'), callback=self.show_recent_downloaded_image, target_path=self.directory_input.get())
 
-        if self.queue_executioner.is_job_done:
-            self.notify_download_complete()
-            self.reset_form()
+        if not self.is_manually_abort:
+            self.queue_executioner.exec_queues(text_input=self.text_editor_input.get(index1=0.0, index2='end-1c'), callback=self.show_recent_downloaded_image, target_path=self.directory_input.get())
+
+            if self.queue_executioner.is_job_done:
+                self.notify_download_complete()
+                self.reset_form()
+            else:
+                self.update_progress_bar()
         else:
-            self.update_progress_bar()
+            self.notify_download_abort()
+            self.soft_reset_form()
 
     def update_progress_bar(self):
         self.progress_bar.config(maximum=self.queue_executioner.end_index)
